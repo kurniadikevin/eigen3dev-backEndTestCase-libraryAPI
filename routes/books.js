1be87913aef0edc48b69a597deca4aa306427f08
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-02-15 22:54:18 +0700

    fix bug conditional book stock for input member book borrowed

--- routes/books.js
+++ routes/books.js
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
 
 
 
-// penalized system
+
 
 //borrow book
 router.post('/borrow-book',function(req,res,next){
@@ -34,13 +34,13 @@ router.post('/borrow-book',function(req,res,next){
       console.log('book borrowed');
 
     // check if book stock is not zero before input to member borrowed book
-    Book.find({'code' : req.body.book_code},{_id: 0})
+    Book.find({code : req.body.book_code},{_id: 0})
     .exec(function (err, book_info) {
       if (err) {
         return next(err);
       }
-      else if (book_info.stock >= 0){
-
+      else  if (book_info[0].stock > 0) {
+        console.log(book_info)
       Member.findOneAndUpdate({code: req.body.member_code, number_of_book_borrowed : {$lt : 2}},{
         $inc : {number_of_book_borrowed : 1}, $push : { 
           borrowed_book : {book_code : req.body.book_code, date : new Date()}
@@ -79,14 +79,34 @@ router.post('/return-book',function(req,res,next){
       }},{
         $inc : {number_of_book_borrowed : -1}, $pull : { 
           borrowed_book : {book_code : req.body.book_code}
-        }
+        },
+        /*
+          RETURN
+        if borrowed_book - > book_ code date now - date borrowed more than 7 days 
+            -set penalized to true
+            - set penalized_date to Date now
+            
+            BORROW
+          - if penalized_date > 3 days set penalized to false and penalized date to null
+        */
       },
       { returnNewDocument: true},
       (err)=>{
         if(err){
           return next(err);
         } 
-        console.log('member info updated for returning book')
+        console.log('member info updated for returning book');
+
+     /*    //penalized after 7 days
+        Member.findOneAndUpdate({ code : req.body.member_code, "borrowed_book.book_code" : {
+          $in : [req.body.book_code]
+        }},{
+          penalized : {
+            $cond: { if: { $eq: [ "$borrowed_book.book_code", req.body.book_code ] }, then: 30, else: 20 }
+          } 
+        }
+        ) */
+
       })
       res
         .status(200)
