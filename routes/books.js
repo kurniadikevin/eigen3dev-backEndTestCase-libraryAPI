0cdc22f2330fcaccb459cfdd236685e3bf64b5fc
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-02-16 13:49:17 +0700

    add middleware for check and clear penalize

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Book = require('../models/book');
 const Member = require('../models/member');
 
-let book_list_available ;
+//let book_list_available ;
 
 /* GET all book available listing. */
 router.get('/books', function(req, res, next) {
@@ -19,11 +19,49 @@ router.get('/books', function(req, res, next) {
 
 
 
+//clearing penalized after 3 days 
+const clearingPenalized =(req,res,next)=>{
+  Member.find({code : req.body.member_code},{_id:0})
+  .exec(function(err, member_info){
+   const penalizedDateInSeconds = member_info[0].penalized_date / 1000;
+   console.log(penalizedDateInSeconds);
+   const currentDateInSeconds= new Date() / 1000;
+   const timeDifferentInSeconds = currentDateInSeconds - penalizedDateInSeconds;
+   
+   if(err){
+    return next(err);
 
+    //should be 3 days 
+   } else if( timeDifferentInSeconds > 30){
+   
+    Member.findOneAndUpdate ({ code : req.body.member_code},{
+      penalized : false, penalized_date : null
+    },{ returnNewDocument: true},
+    (err)=>{
+      if(err){
+        return next(err);
+      }
+      console.log('member penalized removed');
+      next()
+    })
+   } 
+   else{
+      console.log('member still in penalized period');
+     res.send('member still in penalized period')
+    //next();
+   }
+  
+
+   //res.send('200')
+  })
+}
+
+router.post('/test',function(req,res){
+  clearingPenalized(req,res)
+});
 
 //borrow book
-router.post('/borrow-book',function(req,res,next){
-
+router.post('/borrow-book',clearingPenalized ,function(req,res,next){
   
     // check if book stock is not zero before input to member borrowed book
     Book.find({code : req.body.book_code},{_id: 0})
@@ -32,11 +70,14 @@ router.post('/borrow-book',function(req,res,next){
         return next(err);
       }
       else  if (book_info[0].stock !== 0) {
-        console.log(book_info[0])
-      Member.findOneAndUpdate({code: req.body.member_code, number_of_book_borrowed : {$lt : 2}},{
+        console.log(book_info[0]);
+    
+    // increase number of book borrowed on member then push book code and date on borrowed book property on member
+      Member.findOneAndUpdate ({code: req.body.member_code, number_of_book_borrowed : {$lt : 2},
+        penalized : false},{
         $inc : {number_of_book_borrowed : 1}, $push : { 
           borrowed_book : {book_code : req.body.book_code, date : new Date()}
-         ,oldest_borrowed_book : new Date()}
+        }
       },
       { returnNewDocument: true},
       (err)=>{
@@ -65,9 +106,54 @@ router.post('/borrow-book',function(req,res,next){
 })
 
 
+const checkForPenalized =(req,res,next)=>{
+  Member.find({code : req.body.member_code},{_id: 0})
+  .exec(function (err, member_info) {
+   // console.log(member_info)
+    const member_borrowed_book_date_arr = member_info.map((item)=>{
+      return item.borrowed_book;
+    })
+    //console.log( member_borrowed_book_date_arr[0][0].date)
+    const oldest_date_borrowed = member_borrowed_book_date_arr[0][0].date;
+    const oldest_date_borrowed_in_seconds= oldest_date_borrowed /1000 ;
+    console.log(oldest_date_borrowed_in_seconds);
+
+    const current_date_in_seconds= new Date() / 1000;
+    console.log(current_date_in_seconds);
+
+    const timeDifferentInSeconds = current_date_in_seconds - oldest_date_borrowed_in_seconds;
+    if (err) {
+      return next(err);
+    }
+    else if(
+      // should be 7 days
+      timeDifferentInSeconds > 5
+    ){
+    console.log('more than 5s ago');
+    Member.findOneAndUpdate ({ code : req.body.member_code},{
+      penalized : true, penalized_date : new Date()
+    },{ returnNewDocument: true},
+    (err)=>{
+      if(err){
+        return next(err);
+      }
+      console.log('member penalized');
+      next();
+    })
+    
+     }
+  else{
+    console.log('less than 5s ago')
+    next();
+
+  }})
+} 
+
+
+
 
 //return book
-router.post('/return-book',function(req,res,next){
+router.post('/return-book',checkForPenalized,function(req,res,next){
 
   Book.findOneAndUpdate({code: req.body.book_code, borrowed_by : {$in : [req.body.member_code]} },{
     $inc : { stock : 1 }, $pull: {borrowed_by : req.body.member_code}},
@@ -117,6 +203,7 @@ router.post('/return-book',function(req,res,next){
         .end()
     })
 })
+
 
 
 
