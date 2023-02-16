02625179dc6196a878baf641eba0730228875619
Kevin Kurniadi <kurniadikevin@gmail.com>
2023-02-16 14:25:56 +0700

    refactor

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const Book = require('../models/book');
 const Member = require('../models/member');
 
-//let book_list_available ;
 
 /* GET all book available listing. */
 router.get('/books', function(req, res, next) {
@@ -31,8 +30,12 @@ const clearingPenalized =(req,res,next)=>{
    if(err){
     return next(err);
 
+   } else if( member_info[0].penalized === false){
+    console.log('member not penalized');
+    next()
+   }
     //should be 3 days (259200 seconds)
-   } else if( timeDifferentInSeconds > 259200){
+    else if( timeDifferentInSeconds > 259200){
    
     Member.findOneAndUpdate ({ code : req.body.member_code},{
       penalized : false, penalized_date : null
@@ -44,19 +47,20 @@ const clearingPenalized =(req,res,next)=>{
       console.log('member penalized removed');
       next()
     })
-   } 
+    } 
    else{
       console.log('member still in penalized period');
      res.send('member still in penalized period')
     //next();
    }
-
   })
 }
 
+//update penalized manually
 router.post('/update-penalized',function(req,res){
   clearingPenalized(req,res)
 });
+
 
 //borrow book
 router.post('/borrow-book',clearingPenalized ,function(req,res,next){
@@ -96,22 +100,21 @@ router.post('/borrow-book',clearingPenalized ,function(req,res,next){
       })
     }
       res
-        .send('Member info and book stock updated')
+        .send('Member info and book stock updated for borrowing case')
         .status(200)
         .end()
     })
-//})
 })
 
 
+//check for penalized middleware
 const checkForPenalized =(req,res,next)=>{
   Member.find({code : req.body.member_code},{_id: 0})
   .exec(function (err, member_info) {
-   // console.log(member_info)
+
     const member_borrowed_book_date_arr = member_info.map((item)=>{
       return item.borrowed_book;
     })
-    //console.log( member_borrowed_book_date_arr[0][0].date)
     const oldest_date_borrowed = member_borrowed_book_date_arr[0][0].date;
     const oldest_date_borrowed_in_seconds= oldest_date_borrowed /1000 ;
     console.log(oldest_date_borrowed_in_seconds);
@@ -124,10 +127,10 @@ const checkForPenalized =(req,res,next)=>{
       return next(err);
     }
     else if(
-      // should be 7 days
+      // should be 7 days in seconds
       timeDifferentInSeconds > 604800
     ){
-    console.log('more than 5s ago');
+    console.log('more than 7 days ago');
     Member.findOneAndUpdate ({ code : req.body.member_code},{
       penalized : true, penalized_date : new Date()
     },{ returnNewDocument: true},
@@ -141,7 +144,7 @@ const checkForPenalized =(req,res,next)=>{
     
      }
   else{
-    console.log('less than 5s ago')
+    console.log('less than 7 days ago')
     next();
 
   }})
@@ -161,22 +164,12 @@ router.post('/return-book',checkForPenalized,function(req,res,next){
         return next(err);
       }
       console.log('book returned');
-
       Member.findOneAndUpdate({code: req.body.member_code, "borrowed_book.book_code" : {
         $in : [req.body.book_code]
       }},{
         $inc : {number_of_book_borrowed : -1}, $pull : { 
           borrowed_book : {book_code : req.body.book_code}
         },
-        /*
-          RETURN
-        if borrowed_book - > book_ code date now - date borrowed more than 7 days 
-            -set penalized to true
-            - set penalized_date to Date now
-            
-            BORROW
-          - if penalized_date > 3 days set penalized to false and penalized date to null
-        */
       },
       { returnNewDocument: true},
       (err)=>{
@@ -184,19 +177,9 @@ router.post('/return-book',checkForPenalized,function(req,res,next){
           return next(err);
         } 
         console.log('member info updated for returning book');
-
-     /*    //penalized after 7 days
-        Member.findOneAndUpdate({ code : req.body.member_code, "borrowed_book.book_code" : {
-          $in : [req.body.book_code]
-        }},{
-          penalized : {
-            $cond: { if: { $eq: [ "$borrowed_book.book_code", req.body.book_code ] }, then: 30, else: 20 }
-          } 
-        }
-        ) */
-
       })
       res
+        .send('Member info and book stock updated for returning case')
         .status(200)
         .end()
     })
